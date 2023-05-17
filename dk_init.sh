f6f6db4ddb7cbce132c622fc0a2bd9176a116dca
panyanyany <panyanyany@gmail.com>
2023-05-17 14:35:06 +0800

    使用构建好的镜像

--- dk_init.sh
+++ dk_init.sh
@@ -1,4 +1,23 @@
-/wait-for -t 60 http://api:8080 && /wait-for -t 60 http://admin:8000/admin/login && /wait-for -t 60 http://web:3000
+fail() {
+    name=$1
+    echo '----------------------------------------'
+    echo "------------ ${name}启动失败！ ----------"
+    echo '----------------------------------------'
+    exit 1
+}
+
+/wait-for -t 60 http://api:8080 
+if [ $? -ne 0 ]; then
+    fail 'api'
+fi
+/wait-for -t 60 http://admin:8000/admin/login
+if [ $? -ne 0 ]; then
+    fail 'admin'
+fi
+/wait-for -t 60 http://web:3000
+if [ $? -ne 0 ]; then
+    fail 'web'
+fi
 
 if [ $? -eq 0 ]; then
     sleep 5
